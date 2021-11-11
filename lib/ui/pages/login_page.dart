4794626ad24fced82c2796ad7139e03f8abd17d6
Jose David Padilla Gamarra <djgamarra@uninorte.edu.co>
2021-11-11 08:41:48 -0500

    Login scroll

--- lib/ui/pages/login_page.dart
+++ lib/ui/pages/login_page.dart
@@ -60,135 +60,137 @@ class _LoginPageState extends State<LoginPage> {
     const String logoUrl = 'assets/logo_login.svg';
     final mediaQuery = MediaQuery.of(context);
     return Scaffold(
-      body: Form(
-        key: _formKey,
-        child: Container(
-          alignment: Alignment.bottomCenter,
-          height: mediaQuery.size.height,
-          width: mediaQuery.size.width,
-          decoration: const BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage("assets/background.jpg"),
-              fit: BoxFit.fill,
-            ),
-          ),
+      body: SingleChildScrollView(
+        child: Form(
+          key: _formKey,
           child: Container(
-            height: mediaQuery.size.height * .7,
+            alignment: Alignment.bottomCenter,
+            height: mediaQuery.size.height,
             width: mediaQuery.size.width,
             decoration: const BoxDecoration(
-              color: Color(0xAACCDDE7),
-              borderRadius: BorderRadius.only(
-                topRight: Radius.circular(15),
-                topLeft: Radius.circular(15),
+              image: DecorationImage(
+                image: AssetImage("assets/background.jpg"),
+                fit: BoxFit.fill,
               ),
             ),
-            // ignore: sized_box_for_whitespace
             child: Container(
-              width: mediaQuery.size.width * .85,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  SvgPicture.asset(
-                    logoUrl,
-                    semanticsLabel: 'World Builder Logo',
-                    fit: BoxFit.scaleDown,
-                  ),
-                  // ignore: sized_box_for_whitespace
-                  Container(
-                    width: mediaQuery.size.width * .85,
-                    height: mediaQuery.size.height * .5,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: <Widget>[
-                        Column(
-                          children: <Widget>[
-                            const Padding(
-                              padding: EdgeInsets.fromLTRB(0, 0, 0, 4),
-                              child: Align(
-                                alignment: Alignment.centerLeft,
-                                child: Text(
-                                  'Correo electrónico',
-                                  style: TextStyle(fontSize: 16),
-                                ),
-                              ),
-                            ),
-                            TextFormField(
-                              keyboardType: TextInputType.emailAddress,
-                              onChanged: _onFieldChanged('email'),
-                              validator: emailValidator,
-                              decoration: InputDecoration(
-                                border: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(10),
-                                  borderSide: const BorderSide(
-                                    width: 0,
-                                    style: BorderStyle.none,
-                                  ),
-                                ),
-                                filled: true,
-                                fillColor: Colors.white,
-                              ),
-                            ),
-                          ],
-                        ),
-                        Column(
-                          children: <Widget>[
-                            const Padding(
-                              padding: EdgeInsets.fromLTRB(0, 0, 0, 4),
-                              child: Align(
-                                alignment: Alignment.centerLeft,
-                                child: Text(
-                                  'Contraseña',
-                                  style: TextStyle(fontSize: 16),
-                                ),
-                              ),
-                            ),
-                            TextFormField(
-                              keyboardType: TextInputType.visiblePassword,
-                              obscureText: true,
-                              onChanged: _onFieldChanged('password'),
-                              validator: passwordValidator,
-                              decoration: InputDecoration(
-                                border: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(10),
-                                  borderSide: const BorderSide(
-                                    width: 0,
-                                    style: BorderStyle.none,
-                                  ),
-                                ),
-                                filled: true,
-                                fillColor: Colors.white,
-                              ),
-                            ),
-                          ],
-                        ),
-                        ElevatedButton(
-                          onPressed: _onLoginBtnClick,
-                          style: ElevatedButton.styleFrom(
-                            elevation: 0,
-                            primary: const Color(0xFF92D8FF),
-                            fixedSize: Size(mediaQuery.size.width * 0.4, 50),
-                          ),
-                          child: Text(
-                            "LOGIN",
-                            style: GoogleFonts.play(fontSize: 18),
-                          ),
-                        ),
-                        ElevatedButton(
-                          onPressed: _onRegisterBtnClick,
-                          style: ElevatedButton.styleFrom(
-                            elevation: 0,
-                            primary: Colors.white,
-                            fixedSize: Size(mediaQuery.size.width * 0.4, 50),
-                          ),
-                          child: Text(
-                            "REGISTRO",
-                            style: GoogleFonts.play(fontSize: 18),
-                          ),
-                        ),
-                      ],
+              height: mediaQuery.size.height * .7,
+              width: mediaQuery.size.width,
+              decoration: const BoxDecoration(
+                color: Color(0xAACCDDE7),
+                borderRadius: BorderRadius.only(
+                  topRight: Radius.circular(15),
+                  topLeft: Radius.circular(15),
+                ),
+              ),
+              // ignore: sized_box_for_whitespace
+              child: Container(
+                width: mediaQuery.size.width * .85,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    SvgPicture.asset(
+                      logoUrl,
+                      semanticsLabel: 'World Builder Logo',
+                      fit: BoxFit.scaleDown,
                     ),
-                  )
-                ],
+                    // ignore: sized_box_for_whitespace
+                    Container(
+                      width: mediaQuery.size.width * .85,
+                      height: mediaQuery.size.height * .5,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: <Widget>[
+                          Column(
+                            children: <Widget>[
+                              const Padding(
+                                padding: EdgeInsets.fromLTRB(0, 0, 0, 4),
+                                child: Align(
+                                  alignment: Alignment.centerLeft,
+                                  child: Text(
+                                    'Correo electrónico',
+                                    style: TextStyle(fontSize: 16),
+                                  ),
+                                ),
+                              ),
+                              TextFormField(
+                                keyboardType: TextInputType.emailAddress,
+                                onChanged: _onFieldChanged('email'),
+                                validator: emailValidator,
+                                decoration: InputDecoration(
+                                  border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(10),
+                                    borderSide: const BorderSide(
+                                      width: 0,
+                                      style: BorderStyle.none,
+                                    ),
+                                  ),
+                                  filled: true,
+                                  fillColor: Colors.white,
+                                ),
+                              ),
+                            ],
+                          ),
+                          Column(
+                            children: <Widget>[
+                              const Padding(
+                                padding: EdgeInsets.fromLTRB(0, 0, 0, 4),
+                                child: Align(
+                                  alignment: Alignment.centerLeft,
+                                  child: Text(
+                                    'Contraseña',
+                                    style: TextStyle(fontSize: 16),
+                                  ),
+                                ),
+                              ),
+                              TextFormField(
+                                keyboardType: TextInputType.visiblePassword,
+                                obscureText: true,
+                                onChanged: _onFieldChanged('password'),
+                                validator: passwordValidator,
+                                decoration: InputDecoration(
+                                  border: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(10),
+                                    borderSide: const BorderSide(
+                                      width: 0,
+                                      style: BorderStyle.none,
+                                    ),
+                                  ),
+                                  filled: true,
+                                  fillColor: Colors.white,
+                                ),
+                              ),
+                            ],
+                          ),
+                          ElevatedButton(
+                            onPressed: _onLoginBtnClick,
+                            style: ElevatedButton.styleFrom(
+                              elevation: 0,
+                              primary: const Color(0xFF92D8FF),
+                              fixedSize: Size(mediaQuery.size.width * 0.4, 50),
+                            ),
+                            child: Text(
+                              "LOGIN",
+                              style: GoogleFonts.play(fontSize: 18),
+                            ),
+                          ),
+                          ElevatedButton(
+                            onPressed: _onRegisterBtnClick,
+                            style: ElevatedButton.styleFrom(
+                              elevation: 0,
+                              primary: Colors.white,
+                              fixedSize: Size(mediaQuery.size.width * 0.4, 50),
+                            ),
+                            child: Text(
+                              "REGISTRO",
+                              style: GoogleFonts.play(fontSize: 18),
+                            ),
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
               ),
             ),
           ),
